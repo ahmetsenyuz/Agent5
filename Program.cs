2be7ba63054363d57fb8326ad28902cfdd21faf5
ahmetsenyuz <89253941+ahmetsenyuz@users.noreply.github.com>
2026-03-03 22:09:00 +0300

    BI-0001: Create basic Program.cs file for C# console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Agent5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
